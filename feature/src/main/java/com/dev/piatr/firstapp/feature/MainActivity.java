1d4cb04bd60ad94677db1b88f4daddb6aaa042aa
euthanasiolog <piatryxa@gmail.com>
2017-12-02 18:23:57 +0300

    сделаны 2 активности и необходимый функционал

--- feature/src/main/java/com/dev/piatr/firstapp/feature/MainActivity.java
+++ feature/src/main/java/com/dev/piatr/firstapp/feature/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dev.piatr.firstapp.feature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void sendMessage (View view){
+        Intent intent = new Intent(this, SecondActivity.class);
+        EditText message = (EditText) findViewById(R.id.message);
+        String sendMessage = message.getText().toString();
+        intent.putExtra("message", sendMessage);
+        startActivity(intent);
+    }
+
 }
--- feature/src/main/java/com/dev/piatr/firstapp/feature/SecondActivity.java
+++ feature/src/main/java/com/dev/piatr/firstapp/feature/SecondActivity.java
@@ -1,10 +1,23 @@
 package com.dev.piatr.firstapp.feature;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by piatr on 01.12.17.
  */
 
 public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Bundle extras = getIntent().getExtras();
+        TextView textView = new TextView(this);
+        if(extras!=null){
+          String message = extras.getString("message");
+          textView.setText(message);
+        }
+        setContentView(textView);
+    }
 }
